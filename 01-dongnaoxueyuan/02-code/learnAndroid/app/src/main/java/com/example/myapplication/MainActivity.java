c4e42283f36c4605e008cc79ab875646b5222d54
lm101845 <1018457683@qq.com>
2022-12-01 20:01:49 +0800

    第1-10集看完

---
+++ 01-dongnaoxueyuan/02-code/learnAndroid/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("ning","onCreate");
+        //打印调试级别的log
+    }
+}
